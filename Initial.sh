44f669db017ae0f84c5e4b416b6c84e38a2cb7a0
ITXiaoPang <djh1017555@126.com>
2015-09-29 22:05:49 +0800

    Add a script to initial. Modify README.md

---
+++ Initial.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -a OSX_Battery_Charge_Detection.sh /usr/local/bin/
+cp -a ChargeCompleteNotification.app /Applications/
+cp -a com.ITXiaoPang.OSX_Battery_Charge_Detection.plist ~/Library/LaunchAgents
