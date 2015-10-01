aee65d5995a0c57e780ea1e4edf04e2378c5cac9
ITXiaoPang <djh1017555@126.com>
2015-10-01 23:15:53 +0800

    Add some note when running the script

--- OSX_Battery_Charge_Detection.sh
+++ OSX_Battery_Charge_Detection.sh
@@ -7,8 +7,10 @@ do
   if [ "$drawing_from" == "'ACPower'" ]
   then
     battery_percent=$(echo $info|awk '{print $7}')
+    echo $battery_percent
     if [ "$battery_percent" == "100%;" ]
     then
+      echo Call Notification
       open /Applications/ChargeCompleteNotification.app
     fi
   fi
